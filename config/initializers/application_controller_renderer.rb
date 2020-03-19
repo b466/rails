dcab7e2f32056539e37a7f21185c36582afd9baa
、健太 <kenta@kentanoMacBook-Pro.local>
2020-03-19 14:31:53 +0900

    start

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'd6970605db8994cbea9be47185dadde8d92943a1ea2c806323e5992df8697a391d234cf1b0319bf7f25be2a3cd65a9f0c1a4c3f93dff796267c8351de444fbf8'
